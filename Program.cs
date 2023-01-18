91e5149c5cea75870dc084af59c4f65809d849dc
tyuXX <82142326+tyuXX@users.noreply.github.com>
2023-01-18 18:56:59 +0000

    a

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine(x*x);
+    }
+}
